cf841ad0900bbef7f0fff40c9f2342f0d6b36cfd
1PALADIN1 <sonikx94@yandex.ru>
2022-04-22 12:54:37 +0300

    Initial Commit

---
+++ Todoey/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ruslan Malinovsky on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
